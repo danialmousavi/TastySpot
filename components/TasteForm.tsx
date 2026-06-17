a5c26fddef4309665b38f9e5e7fc11cd04c95c1e
danialmousavi <danialmoosavi69@gmail.com>
2026-06-17 17:05:51 +0330

    feat: implement TasteForm component with title input and styling

--- components/TasteForm.tsx
+++ components/TasteForm.tsx
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native'
+import { Colors } from '../constants/colors'
 
 const TasteForm = () => {
+  const [title,setTitle]=useState("")
+  const titleHandler=(value:string)=>{
+    setTitle(value)
+  }
   return (
-    <View>
-      <Text>Form Field</Text>
-    </View>
+    <>
+    <ScrollView style={styles.container}>
+      <View>
+        <Text style={styles.title}>title</Text>
+        <TextInput onChangeText={titleHandler} value={title} style={styles.input}/>
+      </View>
+    </ScrollView>
+    </>
   )
 }
 
 export default TasteForm
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    padding:20,
+
+  },
+  title:{
+    fontSize:20,
+    fontWeight:"bold",
+    color:Colors.primary
+  },
+  input:{
+    marginTop:10,
+    padding:8,
+    borderBottomWidth:3,
+    borderBottomColor:Colors.primary,
+    backgroundColor:Colors.BackGround2,
+    color:Colors.text
+  }
+})
